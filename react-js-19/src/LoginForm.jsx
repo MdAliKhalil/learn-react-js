199cc8d68a0b7fa582205426eff749f93363a36b
Muhammad Ali Khalil <mdalikhalil20@gmail.com>
2025-02-26 23:44:29 +0500

    Login Form Without Page Loading

--- react-js-19/src/LoginForm.jsx
+++ react-js-19/src/LoginForm.jsx
@@ -6,22 +6,25 @@ function LoginForm() {
     const [user, setUser] = useState();
     const [pass, setPass] = useState();
 
-    const formHandler = () => {
+    const formHandler = (event) => {
+        event.preventDefault();
         alert(`Successfully login with username: ${user} and password: ${pass}`);
     }
 
     return (
         <div>
             <h1>Login Form</h1>
-            <input type='text' name="" id="" value={user} placeholder='Enter your email' required className={style.inpt} onChange={(event) => {
-                setUser(event.target.value);
-            }} />
-            <br />
-            <input type="password" name="" id="" value={pass} placeholder='Enter your password' required className={style.inpt} onChange={(event) => {
-                setPass(event.target.value);
-            }} />
-            <br />
-            <button onClick={formHandler} className={style.btn}>Login Now</button>
+            <form onSubmit={formHandler}>
+                <input type='text' name="" id="" value={user} placeholder='Enter your email' required className={style.inpt} onChange={(event) => {
+                    setUser(event.target.value);
+                }} />
+                <br />
+                <input type="password" name="" id="" value={pass} placeholder='Enter your password' required className={style.inpt} onChange={(event) => {
+                    setPass(event.target.value);
+                }} />
+                <br />
+                <input type="submit" value="Login Now" className={style.btn} />
+            </form>
             <Divider />
         </div>
     );
