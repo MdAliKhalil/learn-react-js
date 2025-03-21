fa356fb073d534c7945191acf37a5564873254c1
Muhammad Ali Khalil <mdalikhalil20@gmail.com>
2025-03-21 18:01:26 +0500

    Update Main Route

--- react-js-19/src/main.jsx
+++ react-js-19/src/main.jsx
@@ -2,13 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
